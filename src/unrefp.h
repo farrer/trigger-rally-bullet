a49682695bbd174f6b6fe9f2afbdbf995b68ed97
ab396356 <ab396356@6b6f3af4-9449-45d3-b87a-d38aa902195c>
2015-05-04 19:03:13 +0000

    add UNREFERENCED_PARAMETER macro for all platforms

---
+++ src/unrefp.h
@@ -0,0 +1,12 @@
+///
+/// @file
+/// @license GNU GPL 2
+/// @brief Enables an MS-style macro for use with unused function parameters.
+///
+
+#pragma once
+
+#ifndef UNREFERENCED_PARAMETER
+#define UNREFERENCED_PARAMETER(p)   ((void)(p))
+#endif
+
