0f7b02339d68dab25c49c88b689f0608d3390443
ab396356 <ab396356@6b6f3af4-9449-45d3-b87a-d38aa902195c>
2015-06-17 08:24:04 +0000

    astyle'd

--- src/include/exception.h
+++ src/include/exception.h
@@ -1,31 +1,48 @@
+//
+// Copyright (C) 2004-2006 Jasmine Langridge, ja-reiko@users.sourceforge.net
+// Copyright (C) 2015 Andrei Bondor, ab396356@users.sourceforge.net
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
 
-// exception.h [pengine]
+#pragma once
 
-// Copyright 2004-2006 Jasmine Langridge, jas@jareiko.net
-// License: GPL version 2 (see included gpl.txt)
+#include <exception>
+#include <string>
 
-
-
-class PException : public std::exception
+class PException: public std::exception
 {
 public:
-  PException (const std::string &text) : text_ (text) { }
-  
-  ~PException () throw () { }
-  
-  const char *what () const throw ()
-  {
-    return text_.c_str ();
-  }
-  
+
+    PException(const std::string &text):
+        text(text)
+    {
+    }
+
+    const char *what() const noexcept
+    {
+        return text.c_str();
+    }
+
 private:
-  std::string text_;
+
+    std::string text;
 };
 
+#define makestring2(x)          #x
+#define makestring(x)           makestring2(x)
 
-
-#define makestring2(x) #x
-#define makestring(x) makestring2(x)
-
-#define MakePException(text) PException (std::string () + text + std::string (" at " __FILE__ ":" makestring(__LINE__)))
+#define MakePException(text)    PException(std::string() + text + " at " __FILE__ ":" makestring(__LINE__))
 
