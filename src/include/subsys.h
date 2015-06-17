724774c33a02c7e2c07b8b8ae3fd625ee7ff158e
ab396356 <ab396356@6b6f3af4-9449-45d3-b87a-d38aa902195c>
2015-06-17 13:43:48 +0000

    astyle'd

--- src/include/subsys.h
+++ src/include/subsys.h
@@ -1,76 +1,101 @@
+//
+// Copyright (C) 2004-2006 Jasmine Langridge, ja-reiko@users.sourceforge.net
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
 
-// subsys.h [pengine]
+#pragma once
 
-// Copyright 2004-2006 Jasmine Langridge, jas@jareiko.net
-// License: GPL version 2 (see included gpl.txt)
+#include <string>
+#include <vector>
 
-
-
-class PResource {
+class PResource
+{
 protected:
-  std::string name;
+
+    std::string name;
 
 public:
-  const std::string &getName() { return name; }
+
+    const std::string & getName() const
+    {
+        return name;
+    }
 };
 
 template <class T>
-class PResourceList {
+class PResourceList
+{
 private:
-  std::vector<T *> reslist;
+
+    std::vector<T *> reslist;
 
 public:
-  
-  ~PResourceList() { clear(); }
-  
-  T *add(T *newresource) {
-    reslist.push_back(newresource);
-    return newresource;
-  }
-  T *find(const std::string &name) {
-    for (typename std::vector<T *>::iterator i = reslist.begin(); i != reslist.end(); i++)
-      if (name == (*i)->getName())
-        return *i;
-    return nullptr;
-  }
 
-  void clear() {
-    for (typename std::vector<T *>::iterator i = reslist.begin(); i != reslist.end(); i++) {
-      delete *i;
+    ~PResourceList()
+    {
+        clear();
     }
-    reslist.clear();
-  }
+
+    T * add(T *newresource)
+    {
+        reslist.push_back(newresource);
+        return newresource;
+    }
+
+    T * find(const std::string &name)
+    {
+        for (T *res: reslist)
+            if (name == res->getName())
+                return res;
+
+        return nullptr;
+    }
+
+    void clear()
+    {
+        for (T *res: reslist)
+            delete res;
+
+        reslist.clear();
+    }
 };
 
-
-class PSubsystem {
+class PSubsystem
+{
 protected:
-  PApp &app;
-  
-private:
-  // init() is deprecated, should use RAII now
-  virtual bool init()
-  {
-  	return true;
-  }
+
+    PApp &app;
 
 public:
-  PSubsystem(PApp &parentApp) : app(parentApp)
-  {
-  }
-  
-  virtual ~PSubsystem()
-  {
-  }
 
-  virtual void tick(float delta, const vec3f &eyepos, const mat44f &eyeori, const vec3f &eyevel)
-  {
-  	(void)delta;
-  	(void)eyepos;
-  	(void)eyeori;
-  	(void)eyevel;
-  }
+    PSubsystem(PApp &parentApp):
+        app(parentApp)
+    {
+    }
+
+    virtual ~PSubsystem()
+    {
+    }
+
+    virtual void tick(float delta, const vec3f &eyepos, const mat44f &eyeori, const vec3f &eyevel)
+    {
+        UNREFERENCED_PARAMETER(delta);
+        UNREFERENCED_PARAMETER(eyepos);
+        UNREFERENCED_PARAMETER(eyeori);
+        UNREFERENCED_PARAMETER(eyevel);
+    }
 };
-
-
 
