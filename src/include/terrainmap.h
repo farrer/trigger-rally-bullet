67101b4e7b344ec27eabd0dc24a050b80afb6fcf
onsemeliot <onsemeliot@6b6f3af4-9449-45d3-b87a-d38aa902195c>
2015-08-13 19:22:11 +0000

    added terrain map and codriver commands for 'Aegyptian' and made small adjustments in the terrainmap.h file

--- src/include/terrainmap.h
+++ src/include/terrainmap.h
@@ -1,6 +1,6 @@
 //
 // Copyright (C) 2015 Andrei Bondor, ab396356@users.sourceforge.net
-// & value adaptations from Onsemeliot, onsemeliot@gmail.com
+// Copyright (C) 2015 Onsemeliot, onsemeliot@gmail.com
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -51,8 +51,8 @@ enum TerrainType
     X(RacingTarmac,     RGB( 80,  60, 100),     1.50,       0.00)   \
     X(CountryRoad,      RGB(140,  80, 180),     1.20,       0.00)   \
     X(SlipperyRoad,     RGB(200, 140, 255),     0.50,       0.00)   \
-    X(Ice,              RGB(180, 220, 255),     0.10,       0.00)   \
-    X(DeepSnow,         RGB(255, 255, 255),     0.30,       0.40)   \
+    X(Ice,              RGB(180, 220, 255),     0.20,       0.00)   \
+    X(DeepSnow,         RGB(255, 255, 255),     0.30,       0.50)   \
     X(FirmSand,         RGB(255, 220,   0),     0.90,       0.05)   \
     X(LooseSand,        RGB(255, 255, 140),     0.80,       0.20)   \
     X(PressedEarth,     RGB(255, 200, 200),     1.00,       0.05)   \
@@ -62,4 +62,4 @@ enum TerrainType
     X(FineGravel,       RGB(140, 140, 140),     0.90,       0.10)   \
     X(RoughGravel,      RGB(  0,   0,   0),     0.80,       0.05)   \
     X(Grass,            RGB(  0, 180,   0),     0.40,       0.10)   \
-    X(DeepWater,        RGB(  0,   0, 255),     0.20,       0.40)
+    X(DeepWater,        RGB(  0,   0, 255),     0.20,       0.50)
